5a08a502611ec72a5dab7b6e449a2214b132d4af
Dan Cheeps <105027054+DanDavChicaCheeps67@users.noreply.github.com>
2022-07-24 23:13:45 -0400

    Delete engineTest directory

--- engineTest/helloTest.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <windows.h>
-#include <SDL.h>
-
-int main(int args, char* argvs[])
-{
-	printf("Hello World\n");
-	return 0;
-}
--- engineTest/src/SabrinaEngine/fonts.h
+++
@@ -1,4 +0,0 @@
-#ifndef _FONTS_H
-#define _FONTS_H
-
-#endif
